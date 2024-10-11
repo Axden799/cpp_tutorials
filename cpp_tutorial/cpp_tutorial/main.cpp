8d1eb8c925698a13495641fdd226fb679627b94e
Denis <dedutka@gmail.com>
2024-10-11 11:38:12 -0400

    Finish chapter 4 question 1.

--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -1,23 +1,65 @@
+#include <iomanip> // for std::setw (which sets the width of the subsequent output)
 #include <iostream>
 
-int readNumber()
+double getDouble()
 {
-    int num {};
-    std::cout << "Enter a number: ";
-    std::cin >> num;
-    return num;
+    double x {};
+    std::cout << "Enter a double value: ";
+    std::cin >> x;
+    return x;
 }
 
-void writeAnswer(int answer)
+char getOperator()
 {
-    std::cout << "The answer is: " << answer << '\n';
+    char c {};
+    std::cout << "Enter +, -, *, or /: ";
+    std::cin >> c;
+    if (c == '+' || c == '-' || c == '*' || c== '/')
+    {
+        return c;
+    } else
+    {
+        return 'a';
+    }
+}
+
+double calculateResult(double x, double y, char c)
+{
+    if (c == '+')
+    {
+        return x + y;
+    }
+    else if (c == '-')
+    {
+        return x - y;
+    }
+    else if (c == '*')
+    {
+        return x * y;
+    }
+    else if (c == '/')
+    {
+        return x / y;
+    }
+    else
+    {
+        return 0.0;
+    }
 }
 
 int main()
 {
-    int x { readNumber() };
-    int y { readNumber() };
-    writeAnswer(x + y);
+    double x { getDouble() };
+    double y { getDouble() };
+    char c { getOperator() };
+    if (c == 'a')
+    {
+        std::cout << '\n';
+    }
+    else
+    {
+        std::cout << x << " " << c << " " << y << " is " << calculateResult(x, y, c) << '\n';
+    }
     
     return 0;
 }
