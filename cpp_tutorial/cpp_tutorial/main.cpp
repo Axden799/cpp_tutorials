3a3e2b2b99987215ed18b6d56fa3b4cca83718fd
Denis <dedutka@gmail.com>
2024-10-21 10:27:54 -0400

    Question 8.10.2

--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
+int sumTo(int value)
+{
+    int sum { };
+    for(int i{ value }; i > 0; --i)
+    {
+        sum += i;
+    }
+    
+    return sum;
+}
+
 int main()
 {
-    for(int i{ 0 }; i <= 20; i+=2)
-    {
-        std::cout << i << " ";
-    }
+    std::cout << sumTo(5) << " \n";
 
     return 0;
 }
