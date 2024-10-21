a14511b9dae125e635aa43ab83ec20f2c944995b
Denis <dedutka@gmail.com>
2024-10-21 10:39:17 -0400

    Question 8.10.4

--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -4,25 +4,35 @@ void fizzbuzz(int value)
 {
     for(int i{ 1 }; i <= value; ++i)
     {
-        if (i % 3 == 0 && i % 5 == 0)
+        bool printed { false };
+        if (i % 3 == 0)
         {
-            std::cout << "fizzbuzz\n";
-        } else if (i % 3 == 0)
-        {
-            std::cout << "fizz\n";
-        } else if (i % 5 == 0)
-        {
-            std::cout << "buzz\n";
-        } else
-        {
-            std::cout << i << '\n';
+            printed = true;
+            std::cout << "fizz";
         }
+        if (i % 5 == 0)
+        {
+            printed = true;
+            std::cout << "buzz";
+        }
+        if (i % 7 == 0)
+        {
+            printed = true;
+            std::cout << "pop";
+        }
+        
+        if (!printed)
+        {
+            std::cout << i;
+        }
+        
+        std::cout << '\n';
     }
 }
 
 int main()
 {
-    fizzbuzz(15);
+    fizzbuzz(150);
 
     return 0;
 }
