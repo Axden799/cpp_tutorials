f7ecf55a3f0e3cf6e3a073023759da8fa37f6483
Denis <dedutka@gmail.com>
2024-10-24 10:57:49 -0400

    Chapter 1 review.

--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
 
-// write your add function template here
-template <typename T>
-T add(T x, T y)
+int getNum()
 {
-    return x + y;
-}
-
-template <typename T>
-T mult(T x, int y)
-{
-    return x * y;
-}
-
-auto sub(auto x, auto y)
-{
-    return x - y;
+    int x{};
+    std::cin >> x;
+    return x;
 }
 
 int main()
 {
-    std::cout << add(2, 3) << '\n';
-    std::cout << add(1.2, 3.4) << '\n';
+    std::cout << "Enter an integer: ";
+    int x { getNum() };
+    std::cout << "Enter another integer: ";
+    int y { getNum() };
+    std::cout << x << " " << "+" << " " << y << " is " << x + y << '\n';
+    std::cout << x << " " << "-" << " " << y << " is " << x - y << '\n';
     
-    std::cout << mult(2, 3) << '\n';
-    std::cout << mult(1.2, 3) << '\n';
-    
-    std::cout << sub(3, 2) << '\n';
-    std::cout << sub(3.5, 2) << '\n';
-    std::cout << sub(4, 1.5) << '\n';
-
     return 0;
 }
