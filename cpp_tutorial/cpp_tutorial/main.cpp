f4bc8c2f0e35794369982557cfc2cbcf6acccd6a
Denis <dedutka@gmail.com>
2024-10-24 11:44:15 -0400

    Chapter 3 skipped. Chapter 4 review part 2 of 2.

--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -1,41 +1,44 @@
 #include <iostream>
 
-double getValue()
+double getHeight()
 {
-    std::cout << "Enter a double value: ";
+    std::cout << "Enter the height of the tower in meters: ";
     double x{};
     std::cin >> x;
     return x;
 }
 
-char getSymbol()
+double calculateBallHeight(double towerHeight, int seconds)
 {
-    std::cout << "Enter +, -, *, or /: ";
-    char a{};
-    std::cin >> a;
-    return a;
+    double gravity = 9.8;
+    double distanceFallen = gravity*(seconds*seconds)/2;
+    return towerHeight - distanceFallen;
 }
 
-void calculateResult(double x, double y, char symbol)
+void printBallHeight(double ballHeight, int seconds)
 {
-    std::cout << x << " " << symbol << " " << y << " is ";
-    
-    if (symbol == '+')
-        std::cout << x + y << '\n';
-    if (symbol == '-')
-        std::cout << x - y << '\n';
-    if (symbol == '*')
-        std::cout << x * y << '\n';
-    if (symbol == '/')
-        std::cout << x / y << '\n';
+    if (ballHeight > 0)
+        std::cout << "At " << seconds << " seconds, the ball is at height: " << ballHeight << '\n';
+    else
+        std::cout << "At " << seconds << " seconds, the ball is on the ground." << '\n';
+}
+
+void calculateAndPrintBallHeight(double towerHeight, int seconds)
+{
+    double ballHeight = calculateBallHeight(towerHeight, seconds);
+    printBallHeight(ballHeight, seconds);
 }
 
 int main()
 {
-    double x { getValue() };
-    double y { getValue() };
-    char symbol { getSymbol() };
-    calculateResult(x, y, symbol);
+    double towerHeight { getHeight() };
+    calculateAndPrintBallHeight(towerHeight, 0);
+    calculateAndPrintBallHeight(towerHeight, 1);
+    calculateAndPrintBallHeight(towerHeight, 2);
+    calculateAndPrintBallHeight(towerHeight, 3);
+    calculateAndPrintBallHeight(towerHeight, 4);
+    calculateAndPrintBallHeight(towerHeight, 5);
+
     
     return 0;
 }
