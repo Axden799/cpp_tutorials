7a51582f207eed790606f1b775c2f5e789d0af9c
Denis <dedutka@gmail.com>
2024-10-24 13:41:36 -0400

    Chapter 7 review part 1.

---
+++ cpp_tutorial/cpp_tutorial/constants.h
@@ -0,0 +1,12 @@
+//
+//  constants.h
+//  cpp_tutorial
+//
+//  Created by Denis Dutka on 10/24/24.
+//
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+namespace Constants {
+    inline constexpr int maxClassSize {35};
+}
+#endif
--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -1,41 +1,17 @@
+#include "constants.h"
 #include <iostream>
-#include <string>
-#include <string_view>
-
-// Write the function getQuantityPhrase() here
-constexpr std::string_view getQuantityPhrase(int apples)
-{
-    if (apples < 0)
-        return "negative";
-    if (apples == 0)
-        return "no";
-    if (apples == 1)
-        return "a single";
-    if (apples == 2)
-        return "a couple of";
-    if (apples == 3)
-        return "a few";
-    else
-        return "many";
-}
-
-constexpr std::string_view getApplesPluralized(int apples)
-{
-    return (apples == 1) ? "apple" : "apples";
-}
-
-// Write the function getApplesPluralized() here
 
 int main()
 {
-    constexpr int maryApples { 3 };
-    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+    std::cout << "How many students are in your class? ";
+    int students{};
+    std::cin >> students;
 
-    std::cout << "How many apples do you have? ";
-    int numApples{};
-    std::cin >> numApples;
 
-    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+    if (students > Constants::maxClassSize)
+        std::cout << "There are too many students in this class";
+    else
+        std::cout << "This class isn't too large";
 
     return 0;
 }
