49bd18742a883a3132890baed3e29d467456d478
Denis <dedutka@gmail.com>
2024-11-04 13:04:28 -0500

    Chapter 13.6 quiz.

--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -1,20 +1,62 @@
 #include <iostream>
+#include <string_view>
 
-namespace MonsterType
+enum class Animal
 {
-    enum MonsterType {
-        orc,
-        goblin,
-        troll,
-        ogre,
-        skeleton,
-    };
+    pig,
+    chicken,
+    goat,
+    cat,
+    dog,
+    duck,
+};
+
+constexpr std::string_view getAnimalName(Animal animal)
+{
+    using enum Animal;
+    switch (animal)
+    {
+        case pig: return "pig";
+        case chicken: return "chicken";
+        case goat: return "goat";
+        case cat: return "cat";
+        case dog: return "dog";
+        case duck: return "duck";
+        default: return "???";
+    }
+}
+
+void printNumberOfLegs(Animal animal)
+{
+    std::string_view name { getAnimalName(animal) };
+    int legs {};
+    using enum Animal;
+    switch (animal)
+    {
+        case chicken:
+        case duck:
+            legs = 2;
+            break;
+        case goat:
+        case cat:
+        case dog:
+        case pig:
+            legs = 4;
+            break;
+        default:
+            std::cout << "???";
+            break;
+    }
+    
+    std::cout << "A " << name << " has " << legs << " legs.\n";
 }
 
 int main()
 {
-    MonsterType::MonsterType monster { MonsterType::skeleton };
-    if (monster == MonsterType::skeleton)
-        std::cout << "I am a skeleton!\n";
+    Animal cat { Animal::cat };
+    Animal chicken { Animal::chicken };
+    
+    printNumberOfLegs(cat);
+    printNumberOfLegs(chicken);
     return 0;
 }
