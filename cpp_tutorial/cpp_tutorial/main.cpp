313a17b7c1113538114bd0b3bf6ff0eb7f666e0c
Denis <dedutka@gmail.com>
2024-11-07 14:24:52 -0500

    Chapter 13 Review Q3.

--- cpp_tutorial/cpp_tutorial/main.cpp
+++ cpp_tutorial/cpp_tutorial/main.cpp
@@ -1,48 +1,33 @@
 #include <iostream>
 #include <string_view>
 
-struct Monster
+template <typename T>
+struct Triad
 {
-    enum Type
-    {
-        ogre,
-        dragon,
-        orc,
-        giant_spider,
-        slime,
-    }
-    Type type{};
-    std::string name{};
-    int health{};
+    T first{};
+    T second{};
+    T third{};
 };
 
-constexpr std::string_view getMonsterTypeString(Monster::Type type)
+// If using C++17, we need to provide a deduction guide (not required in C++20)
+// A Triad with three arguments of the same type should deduce to a Triad<T>
+template <typename T>
+Triad(T, T, T) -> Triad<T>;
+
+template<typename T>
+void print(const Triad<T>& triad)
 {
-    switch(type)
-    {
-    case Monster::ogre: return "Ogre";
-    case Monster::dragon: return "Dragon";
-    case Monster::orc: return "Orc";
-    case Monster::giant_spider: return "Giant Spider";
-    case Monster::slime: return "Slime";
-    }
-    
-    return "Unknown";
+    std::cout << "[" << triad.first << ", " << triad.second << ", " << triad.third << "]";
 }
 
-void printMonster(const Monster& monster)
-{
-    std::cout << "This " << getMonsterTypeString(monster.type) <<
-        " is named " << monster.name <<
-        " and has " << monster.health << " health.\n";
-}
 
 int main()
 {
-    Monster ogre{ Monster::ogre, "Torg", 145 };
-    Monster slime{ Monster::slime, "Blurp", 23 };
-    printMonster(ogre);
-    printMonster(slime);
+    Triad t1{ 1, 2, 3 }; // note: uses CTAD to deduce template arguments
+    print(t1);
+
+    Triad t2{ 1.2, 3.4, 5.6 }; // note: uses CTAD to deduce template arguments
+    print(t2);
 
     return 0;
 }
